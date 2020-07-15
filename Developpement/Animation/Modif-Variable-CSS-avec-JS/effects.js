8b226e192e93a08c0d6c7580c9896d0803d37eba
Jerow <56309127+Jerow59@users.noreply.github.com>
2020-07-15 21:50:17 +0200

    Delete effects.js

--- Developpement/Animation/Modif-Variable-CSS-avec-JS/effects.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var pos = document.documentElement;
-pos.addEventListener("mousemove", e => {
-    pos.style.setProperty('--x', e.clientX + "px");
-    pos.style.setProperty('--y', e.clientY + "px");
-})
