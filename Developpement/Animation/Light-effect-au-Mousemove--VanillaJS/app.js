8430d21608cae9d4a8b5d45543630b385ac44099
Jerow <56309127+Jerow59@users.noreply.github.com>
2020-12-21 15:59:44 +0100

    Add files via upload

---
+++ Developpement/Animation/Light-effect-au-Mousemove--VanillaJS/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var pos = document.documentElement;
+pos.addEventListener('mousemove', e => {
+    pos.style.setProperty('--x', e.clientX + 'px')
+    pos.style.setProperty('--y', e.clientY + 'px')
+})
